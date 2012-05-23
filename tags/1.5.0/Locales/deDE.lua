0a11043c57a66dc42a2b7c736a78a412497994db
grdn <grdn@c26fd6b6-a520-4d42-8abc-1dff176476d7>
2012-05-23 19:17:12 +0000

    - Added localization for ptBR - iFriends now may show charnames and zones for non-WoW games (in D3 'Acts')

---
+++ tags/1.5.0/Locales/deDE.lua
@@ -0,0 +1,31 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("iFriends", "deDE")
+if not L then return end
+
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+
+--@do-not-package@
+L["Allycity"] = "Darnassus"
+L["Available columns"] = "Verf\195\188gbare Spalten"
+L["Broadcast"] = "Rundfunk"
+L["By Class"] = "Klasse"
+L["By Difficulty"] = "Schwierigkeit"
+L["By Faction"] = "Fraktion"
+L["By Hostility"] = "Feindlichkeit"
+L["By Threshold"] = "Grenzwert"
+L["Center"] = "Mittig"
+L["Column"] = "Spalte"
+L["Game/Realm"] = "Spiel/Realm"
+L["Hordecity"] = "Orgrimmar"
+L["iFriends provides some pre-layoutet columns for character names, zones, etc. In order to display them in the tooltip, write their names in the desired order into the beneath input."] = "iFriends bietet einige vorgefertigte Spalten an, u. a. f\195\188r Charakternamen, Zonen, etc. Um sie im Tooltip anzuzeigen, musst du die Spalten-Namen in das nachfolgende Textfeld eingeben."
+L["Invalid column name!"] = "Ung\195\188ltiger Spaltenname!"
+L["Justification"] = "Ausrichtung"
+L["Left"] = "Links"
+L["Level"] = "Level"
+L["local friend"] = "Realmfreund"
+L["Note"] = "Notiz"
+L["RealID"] = "RealID"
+L["Right"] = "Rechts"
+L["Show Label"] = "Zeige Titel"
+L["Use Icon"] = "Icon verwenden"
+L["You have no online friends."] = "Derzeit sind keine Freunde online."
+--@end-do-not-package@
