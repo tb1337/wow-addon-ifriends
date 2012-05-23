b5146fe91190e4fb51dba2b1e6463d6667392c51
grdn <grdn@c26fd6b6-a520-4d42-8abc-1dff176476d7>
2012-05-23 19:06:37 +0000

    Fixed a bug in pkgmeta

---
+++ trunk/Locales/ptBR.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("iFriends", "ptBR")
+if not L then return end
+
+--@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
