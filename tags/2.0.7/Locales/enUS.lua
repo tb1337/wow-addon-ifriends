aed8162d3ddbc954b2237e6d77da51d985628cd0
grdn <grdn@c26fd6b6-a520-4d42-8abc-1dff176476d7>
2012-08-30 13:47:20 +0000

    See rev 23

---
+++ tags/2.0.7/Locales/enUS.lua
@@ -0,0 +1,46 @@
+local AddonName = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "enUS", true)
+
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+
+--@do-not-package@
+L["Displays the RealID of your Battle.net friends."] = true
+L["Displays the logged on realm of your Battle.net friends."] = true
+L["Displays the level of your friends."] = true
+L["Displays the name of your friends. In addition, a short info is shown if they are AFK or DND."] = true
+L["Displays the race of your Battle.net friends."] = true
+L["Displays the zone of your friends."] = true
+L["Displays the individual note of your friends."] = true
+L["Displays the class of your friends. Choose whether to show the class name or the class icon."] = true
+L["Displays the last broadcast message of your Battle.net friends."] = true
+
+L["Addon update available!"] = true
+
+L["General Options"] = true
+L["Display WoW Friends in another Tooltip"] = true
+L["Display the number of your Battle.net friends on the plugin"] = true
+L["Tooltip Options"] = true
+
+L["RealID"] = true
+L["Game/Realm"] = true
+L["Note"] = true
+L["Broadcast"] = true
+
+L["Column"] = true
+L["Show Label"] = true
+L["Justification"] = true
+L["Left"] = true
+L["Center"] = true
+L["Right"] = true
+L["Use Icon"] = true
+
+L["iFriends provides some pre-layoutet columns for character names, zones, etc. In order to display them in the tooltip, write their names in the desired order into the beneath input."] = true
+L["Invalid column name!"] = true
+L["Available columns"] = true
+
+L["By Difficulty"] = true
+L["By Threshold"] = true
+L["By Hostility"] = true
+L["By Class"] = true
+L["By Faction"] = true
+--@end-do-not-package@
