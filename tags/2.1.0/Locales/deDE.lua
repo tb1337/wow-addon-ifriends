4c293bb0fbd169819b656df6996fdb3414e1d6ac
grdn <grdn@c26fd6b6-a520-4d42-8abc-1dff176476d7>
2012-12-02 21:59:09 +0000

    See rev 31

---
+++ tags/2.1.0/Locales/deDE.lua
@@ -0,0 +1,45 @@
+local AddonName = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "deDE")
+
+if not L then return end
+
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+
+--@do-not-package@
+L["Addon update available!"] = "Addon Update verf\195\188gbar!"
+L["Available columns"] = "Verf\195\188gbare Spalten"
+L["Broadcast"] = "Rundfunk"
+L["By Class"] = "Klasse"
+L["By Difficulty"] = "Schwierigkeit"
+L["By Faction"] = "Fraktion"
+L["By Hostility"] = "Feindlichkeit"
+L["By Threshold"] = "Grenzwert"
+L["Center"] = "Mittig"
+L["Column"] = "Spalte"
+L["Displays the class of your friends. Choose whether to show the class name or the class icon."] = "Zeigt die Klasse deiner Freunde an. W\195\164hle, ob der Klassenname oder das Klassensymbol angezeigt werden soll."
+L["Displays the individual note of your friends."] = "Zeigt die Notiz von deinen Freunden an."
+L["Displays the last broadcast message of your Battle.net friends."] = "Zeigt die letzte Rundfunknachricht deiner Battle.net Freunde an."
+L["Displays the level of your friends."] = "Zeigt die Stufe deiner Freunde an."
+L["Displays the logged on realm of your Battle.net friends."] = "Zeigt den Realm deiner Battle.net Freunde an. Spielen sie nicht WoW, wird stattdessen das Spiel angezeigt."
+L["Displays the name of your friends. In addition, a short info is shown if they are AFK or DND."] = "Zeigt den Namen deiner Freunde an und zus\195\164tzlich einen Hinweis, ob derjenige AFK oder DND ist."
+L["Displays the race of your Battle.net friends."] = "Zeigt das Volk deiner Battle.net Freunde an."
+L["Displays the RealID of your Battle.net friends."] = "Zeigt den echten Namen deiner Battle.net Freunde an."
+L["Displays the zone of your friends."] = "Zeigt das Gebiet deiner Freunde an."
+L["Display the number of your Battle.net friends on the plugin"] = "Zeigt die Anzahl der Battle.net Freunde auf dem Plugin"
+L["Display WoW Friends in another Tooltip"] = "Zeige WoW Freunde in einem weiteren Tooltip"
+L["Displays the BattleTag of your Battle.net friends."] = "Zeigt den BattleTag deiner Battle.net Freunde an."
+L["Enable Script"] = "Aktiviere Skript"
+L["Game/Realm"] = "Spiel/Realm"
+L["General Options"] = "Allgemeine Optionen"
+L["If activated, clicking on the given cell will result in something special."] = "Wenn diese Option aktiviert ist, wird mit einem Klick auf diese Zelle eine besondere Aktion ausgef\195\188hrt."
+L["iFriends provides some pre-layoutet columns for character names, zones, etc. In order to display them in the tooltip, write their names in the desired order into the beneath input."] = "iFriends bietet einige vorgefertigte Spalten an, u. a. f\195\188r Charakternamen, Zonen, etc. Um sie im Tooltip anzuzeigen, musst du die Spalten-Namen in das nachfolgende Textfeld eingeben."
+L["Invalid column name!"] = "Ung\195\188ltiger Spaltenname!"
+L["Justification"] = "Ausrichtung"
+L["Left"] = "Links"
+L["Note"] = "Notiz"
+L["RealID"] = "RealID"
+L["Right"] = "Rechts"
+L["Show Label"] = "Zeige Titel"
+L["Tooltip Options"] = "Tooltip Optionen"
+L["Use Icon"] = "Symbol verwenden"
+--@end-do-not-package@
