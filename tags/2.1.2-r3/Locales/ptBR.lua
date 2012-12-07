55d9993b2258574a0dfdbb8418174784eeae6fa3
grdn <grdn@c26fd6b6-a520-4d42-8abc-1dff176476d7>
2012-12-07 13:56:36 +0000

    See rev 39

---
+++ tags/2.1.2-r3/Locales/ptBR.lua
@@ -0,0 +1,6 @@
+local AddonName = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "ptBR")
+
+if not L then return end
+
+--@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
