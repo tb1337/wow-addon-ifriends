ded593588f3e3c78451ca9a34bde99244255088c
grdn <grdn@c26fd6b6-a520-4d42-8abc-1dff176476d7>
2012-12-07 06:42:22 +0000

    Error

--- tags/2.1.2/Locales/frFR.lua
+++
@@ -1,6 +0,0 @@
-local AddonName = ...;
-local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "frFR")
-
-if not L then return end
-
---@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
--- tags/2.1.2/Locales/zhCN.lua
+++
@@ -1,6 +0,0 @@
-local AddonName = ...;
-local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "zhCN")
-
-if not L then return end
-
---@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
