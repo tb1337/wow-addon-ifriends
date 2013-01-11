fd9499e0dd7dd97c6a55684ba139b709c7c70fe7
grdn <grdn@c26fd6b6-a520-4d42-8abc-1dff176476d7>
2013-01-11 12:23:57 +0000

    See r42

---
+++ tags/2.1.3/Locales/itIT.lua
@@ -0,0 +1,6 @@
+local AddonName = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "itIT")
+
+if not L then return end
+
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
