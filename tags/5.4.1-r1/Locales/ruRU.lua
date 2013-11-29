8a159eb71fdeb322b7b758ed4d1eb23a2c8d1347
grdn <grdn@c26fd6b6-a520-4d42-8abc-1dff176476d7>
2013-11-29 14:37:08 +0000

    See r50

---
+++ tags/5.4.1-r1/Locales/ruRU.lua
@@ -0,0 +1,6 @@
+local AddonName = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "ruRU")
+
+if not L then return end
+
+--@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
